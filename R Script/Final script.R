ea2254a178a199e4134038f4d3aa5f8e5897e85b
Kortney Ervin <kaervin@emory.edu>
2024-08-27 18:48:58 -0400

    fixed table 1 code

--- R Script/Final script.R
+++ R Script/Final script.R
@@ -6,8 +6,6 @@ medal_cols <- c("id", "name", "sex", "age", "height", "weight", "team", "NOC", "
 								"year", "season", "city", "sport", "event", "medal")
 medals <- read_csv(here::here("data", "raw","athlete_events.csv"),
 									 skip = 1, col_names = medal_cols)
-keep.variables <- c("name", "sex", "height", "weight", "NOC", "season", "sport", "event", "medal")
-medals2 <- medals[1:12000, keep.variables]
 
 tbl_summary(
 	medals,
@@ -26,3 +24,5 @@ tbl_summary(
 	bold_labels()|>
 	modify_footnote(update = everything() ~ NA)|>
 	modify_header(label = "**Variable**", p.value = "**P**")
+
+
