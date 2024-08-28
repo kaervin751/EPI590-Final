068d1ecb429c6bd2ddb105d2b09b626b3f29577c
Kortney Ervin <kaervin@emory.edu>
2024-08-28 18:33:22 -0400

    added a readme document and updated rscript

--- R Script/Final script.R
+++ R Script/Final script.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(here)
 library(gtsummary)
-
+library(ggplot2)
 medal_cols <- c("id", "name", "sex", "age", "height", "weight", "team", "NOC", "games",
 								"year", "season", "city", "sport", "event", "medal")
 medals <- read_csv(here::here("data", "raw","athlete_events.csv"),
@@ -33,11 +33,22 @@ tbl_regression(
 	intercept = TRUE)
 
 #creating a histogram
-hist(medals$height,
+histogram<- ggplot(medals, aes(x=height))+
+	geom_histogram(binwidth = 10, fill = "white", color = "black") +
+	theme_minimal() +
+	labs(title = "Distribution of Heights", x = "Height (cm)", y = "Frequency")
+
+
+histogram2<- hist(medals$height,
 		 main = "Frequency of height of olympians",
 		 xlab = "Height in cm",
 		 ylab = "Frequency"
 		 )
+
+ggsave("histogram.png", plot=histogram, width = 8, height = 6, dpi=100)
+
+ggsave(plot = histogram,
+			 filename = here::here("docs", "histogram.png"))
 
 #Question 4 - write a function
 x <- medals$height
---
+++ docs/README.md
@@ -0,0 +1,4 @@
+Run Final script.R then you can render the Quarto-Final-Document. 
+
+there are two different histograms because one looked better in the quarto document, while the other worked better to save
+using the here package
